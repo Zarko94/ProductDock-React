324977b83cd586ab0d6b8b79eaf64e51a3577936
[zarko milanovic] <[zarko9zaka4@yahoo.com]>
2022-03-20 18:34:22 +0100

    first react

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+} from "react-router-dom";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import { logout } from "./service/auth";
+import Login from "./components/authorization/Login";
+import Festivali from "./components/tasks/Festivali";
+import Create from "./components/tasks/Create";
+import Reserve from "./components/tasks/Reserve";
+
+function App() {
+  const jwt = window.localStorage["jwt"];
+
+  if (jwt) {
+    return (
+      <div>
+        <Router>
+          <Navbar expand bg="dark" variant="dark">
+            <Navbar.Brand as={Link} to="/">
+              Zarko
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/festivali">
+                Festivali
+              </Nav.Link>
+            </Nav>
+            <Button onClick={() => logout()}>Logout</Button>
+          </Navbar>
+          <Container style={{ paddingTop: "10px" }}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" render={() => <Redirect to="/" />} />
+              <Route exact path="/festivali" component={Festivali} />
+              <Route exact path="/festivali/create" component={Create} />
+              <Route exact path="/festivali/reserve/:id" component={Reserve} />
+              <Route component={NotFound} />
+            </Switch>
+          </Container>
+        </Router>
+      </div>
+    );
+  } else {
+    return (
+      <Container>
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route render={() => <Redirect to="/login" />} />
+          </Switch>
+        </Router>
+      </Container>
+    );
+  }
+}
+
+export default App;
